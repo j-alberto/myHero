c4792852362e7666be2f311b4c24ff5d429ff5d1
Jorge Rojas <jalberto.roj@gmail.com>
2016-11-17 10:54:27 -0600

    add navigation with routing. Create dashboard component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  imports:      [ 
+  	BrowserModule,
+  	FormsModule,
+  	RouterModule.forRoot([
+      {
+		path: '',
+		redirectTo: '/dashboard',
+	    pathMatch: 'full'
+	  },{
+		path: 'dashboard',
+	  	component: DashboardComponent
+	  },{
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])],
+  declarations: [
+  	AppComponent,
+  	HeroesComponent,
+  	HeroDetailComponent,
+  	DashboardComponent
+  ],
   bootstrap: [AppComponent],
   providers: [HeroService]
 })
+
 export class AppModule { }
