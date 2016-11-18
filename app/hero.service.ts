e6f4221082e943557dbe29ed809b3c039b2349ac
Jorge Rojas <jalberto.roj@gmail.com>
2016-11-18 15:44:48 -0600

    Full CRUD operations to a server

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,6 +8,7 @@ import 'rxjs/add/operator/toPromise';
 export class HeroService {
 	
 	private heroesUrl = 'app/heroes';  // URL to web api
+	private headers = new Headers({'Content-Type': 'application/json'});
 
 	constructor(private http: Http){}
 
@@ -34,4 +35,30 @@ export class HeroService {
 	  return this.getHeroes()
 	             .then(heroes => heroes.find(hero => hero.id === id));
 	}
+
+	updateHero(hero:Hero){
+		const url = `${this.heroesUrl}/${hero.id}`;
+
+		return this.http.put(url, JSON.stringify(hero),{headers: this.headers})
+			.toPromise()
+			.then(()=> hero)
+			.catch(this.handleError);
+	}
+
+	createHero(name: string): Promise<Hero> {
+	  return this.http
+	    .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+	    .toPromise()
+	    .then(res => res.json().data)
+	    .catch(this.handleError);
+	}
+
+	delete(id: number): Promise<void> {
+	  const url = `${this.heroesUrl}/${id}`;
+	  return this.http.delete(url, {headers: this.headers})
+	    .toPromise()
+	    .then(() => null)
+	    .catch(this.handleError);
+	}
+
 }
